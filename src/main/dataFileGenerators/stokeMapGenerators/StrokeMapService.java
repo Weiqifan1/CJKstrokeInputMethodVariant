239c1e18ff08a53d1ff492aa88c5f98ed14e974c
Lykke <cmlykke@hotmail.com>
2023-10-14 23:20:20 +0200

    removing unneeded code

--- src/main/dataFileGenerators/stokeMapGenerators/StrokeMapService.java
+++ src/main/dataFileGenerators/stokeMapGenerators/StrokeMapService.java
@@ -1,12 +1,6 @@
 package main.dataFileGenerators.stokeMapGenerators;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,36 +9,10 @@ import static main.dataFileGenerators.staticDataFileReaders.CodepointCharacterSe
 
 public class StrokeMapService {
 
-    public String test = "123";
-
-
-
     public Set<String> codePointCharacterSequencyRawLine() throws IOException {
         List<String> fileData = readCodepointCharacterSequency();
-
         List<String> compliantLines = fileData.stream().filter(line -> lineMatchPrescribedRegex(line)).toList();
-
         List<String> nonCompliant = fileData.stream().filter(line -> !lineMatchPrescribedRegex(line)).toList();
-
-        List<String> hasBackslahs = fileData.stream().filter(line -> lineHasBackslash(line)).toList();
-        List<String> linestoFil = new ArrayList<>();
-        int line = 1;
-        for (String lin : hasBackslahs) {
-            linestoFil.add(line + " " + lin);
-            line++;
-        }
-
-        try {
-            // Create a Path object representing the file where you want to write into
-            Path out = Paths.get("backslashLines.txt");
-            // Create the actual file, and fill it with the text in the ArrayList
-            Files.write(out, linestoFil, Charset.defaultCharset());
-        } catch (IOException e) {
-            System.out.println(e);
-
-        }
-
-
         return compliantLines.stream().collect(Collectors.toSet());
     }
 
@@ -64,44 +32,18 @@ public class StrokeMapService {
         if (lineSplit.size() < 3) {
             return false;
         }
+
         String codepoint = lineSplit.get(0);
         String charAndSetmarker = lineSplit.get(1);
         String strokes = lineSplit.get(2);
 
         boolean codepointStartWithUplus = codepoint.startsWith("U+");
-        boolean strokesHasBackslash = strokes.matches("[\\\\]+");
-
         boolean strokesMatch = strokes.matches("[1-5|\\\\()]+");
-
         if (!(codepointStartWithUplus && strokesMatch)) {
             return false;
         }
         return true;
     }
 
-    private boolean lineHasBackslash(String inputLine) {
-        List<String> lineSplit = List.of(inputLine.split("\t"));
-
-        if (lineSplit.size() < 3) {
-            return false;
-        }
-        String codepoint = lineSplit.get(0);
-        String charAndSetmarker = lineSplit.get(1);
-        String strokes = lineSplit.get(2);
-
-        boolean codepointStartWithUplus = codepoint.startsWith("U+");
-        boolean strokesHasBackslash = strokes.contains("\\");
-
-        boolean strokesMatch = strokes.matches("[1-5|\\\\()]+");
-
-        if (!(codepointStartWithUplus && strokesMatch)) {
-            return false;
-        }
-
-        if (!strokesHasBackslash) {
-            return false;
-        }
-        return true;
-    }
 }
 
