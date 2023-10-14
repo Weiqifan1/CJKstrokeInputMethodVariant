13c2fad47cc298d63c8256e02e2f0a69121d717e
Lykke <cmlykke@hotmail.com>
2023-10-14 16:32:21 +0200

    added empty reader and service files for codepointCharacterSequency static data file

---
+++ src/dataFileGenerators/staticDataFileReaders/CodepointCharacterSequenceReader.java
@@ -0,0 +1,11 @@
+package dataFileGenerators.staticDataFileReaders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodepointCharacterSequenceReader {
+    public static List<String> getCodepointCharacterSequencyLine() {
+        return new ArrayList<>();
+    }
+
+}
---
+++ src/dataFileGenerators/stokeMapGenerators/StrokeMapService.java
@@ -0,0 +1,10 @@
+package dataFileGenerators.stokeMapGenerators;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StrokeMapService {
+    public static Set<String> codePointCharacterSequencyRawData() {
+        return new HashSet<>();
+    }
+}
