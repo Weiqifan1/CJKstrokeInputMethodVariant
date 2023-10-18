d99678416d90b4df22312e6d6dc026b5d272cb43
Lykke <cmlykke@hotmail.com>
2023-10-18 20:22:43 +0200

    added conwayCode attributes and code to Char class

--- src/main/Models/CJKChaaar.java
+++ src/main/Models/CJKChaaar.java
@@ -1,5 +1,9 @@
 package main.Models;
 
+import main.dataFileGenerators.stokeMapGenerators.ConwayCodeService;
+
+import java.util.Set;
+
 public class CJKChaaar {
     private String CJK;
     private String CJKWithSetMark;
@@ -7,14 +11,25 @@ public class CJKChaaar {
     private String ConwayCode;
     private CJKfrequency junda;
     private CJKfrequency tzai;
+    private Set<String> fullCode;
+    private Set<String> twotwoCode;
+    private Set<String> threethreeCode;
+    private Set<String> twofourCode;
 
-    public CJKChaaar(String CJK, String CJKWithSetMark, String unicodeHex, String conwayCode, CJKfrequency junda, CJKfrequency tzai) {
+
+    public CJKChaaar(String CJK,
+                     String CJKWithSetMark,
+                     String unicodeHex,
+                     String conwayCode,
+                     CJKfrequency junda,
+                     CJKfrequency tzai) {
         this.CJK = CJK;
         this.CJKWithSetMark = CJKWithSetMark;
         this.UnicodeHex = unicodeHex;
         this.ConwayCode = conwayCode;
         this.junda = junda;
         this.tzai = tzai;
+        generateCodesFromConwayCode(conwayCode);
     }
 
     public boolean isMultiSet() {
@@ -43,5 +58,30 @@ public class CJKChaaar {
 
     public CJKfrequency getTzai() {
         return tzai;
+    }
+
+    public Set<String> getFullCode() {
+        return fullCode;
+    }
+
+    public Set<String> getTwotwoCode() {
+        return twotwoCode;
+    }
+
+    public Set<String> getThreethreeCode() {
+        return threethreeCode;
+    }
+
+    public Set<String> getTwofourCode() {
+        return twofourCode;
+    }
+
+    private void generateCodesFromConwayCode(String conwayCode) {
+        ConwayCodeService cs = new ConwayCodeService();
+        Set<String> fullCode = cs.codesFromConway(conwayCode);
+        this.fullCode = fullCode;
+        this.twotwoCode = cs.fullCodesToTwoTwoCodes(fullCode);
+        this.threethreeCode = cs.fullCodeToThreeThree(fullCode);
+        this.twofourCode = cs.fullCodeToTwoFour(fullCode);
     }
 }
